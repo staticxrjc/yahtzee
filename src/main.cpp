af0d7c01765ef4a621c1c097e7999f05dd7861d3
staticxrjc <staticxrjc@gmail.com>
2021-09-25 23:50:01 -0700

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,31 @@ int main()
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
+    sf::Event event;
 
     while (window.isOpen())
     {
-        sf::Event event;
+        // Event Loop
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            switch (event.type) {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::KeyPressed:
+                    if (event.key.code == sf::Keyboard::Escape)
+                        window.close();
+                    break;
+            }
         }
 
+        // Update
         window.clear();
         window.draw(shape);
+
+        //Render
         window.display();
+
     }
 
     return 0;
